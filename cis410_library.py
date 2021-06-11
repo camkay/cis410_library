51c4e56f3d70a4145a9967cb2ac794f33a2493ea
camkay <ckay@uoregon.edu>
2021-06-11 15:18:58 -0700

    Update cis410_library.py

--- cis410_library.py
+++ cis410_library.py
@@ -44,3 +44,6 @@ def mcc(*, tp, tn, fp, fn):
   denom = (tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)
   mcc_score = 0 if denom == 0 else (tp * tn - fp * fn) / denom**.5
   return mcc_score
+
+def accuracy(*, tp, tn, fp, fn):
+  return (tp + tn) / (tp + tn + fp + fn)
